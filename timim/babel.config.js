97775179cc3db6db19ca568a57e43ed4cc6083e6
zhang-yong-qiang <1094093944@qq.com>
2023-03-14 23:51:48 +0800

    feat:babel格式限制

--- timim/babel.config.js
+++ timim/babel.config.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: zhang-yong-qiang 1094093944@qq.com
+ * @Date: 2023-02-26 15:45:45
+ * @LastEditors: zhang-yong-qiang 1094093944@qq.com
+ * @LastEditTime: 2023-03-14 22:44:26
+ * @FilePath: \LCMIM\TIM-IM\timim\babel.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
+    presets: [
+        '@vue/cli-plugin-babel/preset',
+
+    ]
 }
