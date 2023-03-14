890c666870df4425ee245d5e28cfbec9257fa84e
zhang-yong-qiang <1094093944@qq.com>
2023-03-14 23:50:45 +0800

    fix:修改app布局ui

--- timim/src/App.vue
+++ timim/src/App.vue
@@ -2,7 +2,7 @@
  * @Author: zhang-yong-qiang 1094093944@qq.com
  * @Date: 2023-02-26 15:45:45
  * @LastEditors: zhang-yong-qiang 1094093944@qq.com
- * @LastEditTime: 2023-02-26 15:53:45
+ * @LastEditTime: 2023-03-14 22:28:59
  * @FilePath: \LCMIM\TIM-IM\timim\src\App.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -10,6 +10,15 @@
   <router-view/>
 </template>
 
-<style lang="scss">
-
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+}
 </style>
